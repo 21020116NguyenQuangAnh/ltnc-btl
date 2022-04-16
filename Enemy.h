2b595ae6ecda93041fb8505ff464cc20e02b4dac
QuangAnh22112003 <quanganhbh1@gmail.com>
2022-04-16 15:56:35 +0700

    Update

--- Enemy.h
+++ Enemy.h
@@ -14,33 +14,10 @@ const int place[] = {100,300,500};
 struct Enemy{
     int x;
     int y;
-
     SDL_Rect eRect;
 
-    void setpos(int a, int b)
-    {
-        x = a;
-        y = b;
-    }
-
-    void render(SDL_Renderer* renderer, SDL_Texture* enemyTexture, SDL_Rect &eRect)
-    {
-            eRect.x = x;
-            eRect.y = y;
-            eRect.w = ENEMY_WIDTH;
-            eRect.h = ENEMY_HEIGHT;
-            SDL_RenderCopy( renderer, enemyTexture, NULL, &eRect);
-    }
-
-    void move(int score)
-    {
-        x-=5 + (score/500);
-                    if(x < 0)
-                    {
-                    int random = rand()%3;
-                    y = place[random];
-                    x = 1200;
-                    }
-    }
+    void setpos(int _x, int _y);
+    void render(SDL_Renderer* renderer, SDL_Texture* enemyTexture, SDL_Rect &eRect);
+    void move(int score);
 };
 #endif // ENEMY_H
--- Text.h
+++ Text.h
@@ -10,36 +10,7 @@ struct Text{
     SDL_Color Color = {255, 255, 255};
     string content;
 
-    SDL_Texture* loadFromRenderedText( TTF_Font* fontText, SDL_Renderer* renderer )
-    {
-        SDL_Texture* newTexture = NULL;
-        SDL_Surface* textSurface = TTF_RenderText_Solid( fontText , content.c_str(), Color );
-        if( textSurface == NULL )
-        {
-            cout<< "Unable to render text surface! SDL_ttf Error:" <<endl;
-            TTF_GetError();
-        }
-        else
-        {
-            newTexture = SDL_CreateTextureFromSurface( renderer, textSurface );
-            if( newTexture == NULL )
-            {
-                cout<< "Unable to create texture from rendered text! SDL Error:" <<endl;
-                SDL_GetError();
-            }
-            SDL_FreeSurface( textSurface );
-        }
-        return newTexture;
-    }
-
-    void render (SDL_Renderer* renderer, SDL_Texture* textTexture, SDL_Rect &Rect)
-    {
-        SDL_QueryTexture(textTexture, nullptr, nullptr, &Rect.w, &Rect.h);
-        Rect.x = (SCREEN_WIDTH - Rect.w) / 2;
-        Rect.y = y;
-        Rect.w = Rect.w;
-        Rect.h = Rect.h;
-        SDL_RenderCopy (renderer, textTexture, NULL, &Rect);
-    }
+    SDL_Texture* loadFromRenderedText( TTF_Font* fontText, SDL_Renderer* renderer );
+    void render (SDL_Renderer* renderer, SDL_Texture* textTexture, SDL_Rect &Rect);
 };
 #endif // TEXT_H
